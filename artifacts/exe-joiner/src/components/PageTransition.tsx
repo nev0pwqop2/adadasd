374099967a24405f9665574ea007fcae91b971bd
nev0pwqop2 <143802627+nev0pwqop2@users.noreply.github.com>
2026-05-08 18:21:36 +0300

    Delete artifacts/exe-joiner directory

--- artifacts/exe-joiner/src/components/PageTransition.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface PageTransitionProps {
-  children: React.ReactNode;
-}
-
-export default function PageTransition({ children }: PageTransitionProps) {
-  return <>{children}</>;
-}
